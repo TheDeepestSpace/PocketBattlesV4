fd41f280d4e8473fc82652b946f939c33ad7fc32
Boris <borismilkman@gmail.com>
2016-08-17 14:54:38 +0300

    Added Game class :D

---
+++ core/src/com/pocketbattles/game/Game.java
@@ -0,0 +1,42 @@
+package com.pocketbattles.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+/**
+ * Created by Boris on 16.08.2016.
+ * Project: PocketBattlesV4
+ */
+public class Game {
+    public static Stage stage;
+
+    /** INITIALISING */
+
+    public static void initialise() {
+        stage = new Stage();
+    }
+
+    /** CREATING AND SETTING UP */
+
+    public static void create() {
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    /** UPDATING */
+
+    public static void update() {
+
+    }
+
+    /** RENDERING */
+
+    public static void render() {
+
+    }
+
+    /** DISPOSING */
+
+    public static void dispose() {
+        stage.dispose();
+    }
+}
