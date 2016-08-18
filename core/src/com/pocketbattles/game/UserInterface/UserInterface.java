65e2280e71942de8c45ee16ebf25b7fbf5dbbde6
Boris <borismilkman@gmail.com>
2016-08-18 04:42:40 +0300

    Implemented UserInterface class :D

---
+++ core/src/com/pocketbattles/game/UserInterface/UserInterface.java
@@ -0,0 +1,38 @@
+package com.pocketbattles.game.UserInterface;
+
+/**
+ * Created by Boris on 16.08.2016.
+ * Project: PocketBattlesV4
+ */
+public class UserInterface {
+
+    /** INITIALISING */
+
+    public static void initialise() {
+
+    }
+
+    /** CREATING AND SETTING UP */
+
+    public static void create() {
+
+    }
+
+    /** UPDATING */
+
+    public static void update() {
+
+    }
+
+    /** RENDERING */
+
+    public static void render() {
+
+    }
+
+    /** DISPOSING */
+
+    public static void dispose() {
+
+    }
+}
