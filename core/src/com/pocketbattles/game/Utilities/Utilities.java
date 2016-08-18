a46700ee81122e2f78ca2673a00fde462d57bf56
Boris <borismilkman@gmail.com>
2016-08-18 04:34:03 +0300

    Added Utilities class :D

---
+++ core/src/com/pocketbattles/game/Utilities/Utilities.java
@@ -0,0 +1,32 @@
+package com.pocketbattles.game.Utilities;
+
+/**
+ * Created by Boris on 16.08.2016.
+ * Project: PocketBattlesV4
+ */
+public class Utilities {
+
+    /** INITIALISING */
+
+    public static void initialise() {
+
+    }
+
+    /** CREATING AND SETTING UP */
+
+    public static void create() {
+
+    }
+
+    /** UPDATING */
+
+    public static void update() {
+
+    }
+
+    /** DISPOSING */
+
+    public static void dispose() {
+
+    }
+}
