e85b61b685f0672b88f4c649e85cd63aa0998e53
Boris <borismilkman@gmail.com>
2016-08-18 04:36:27 +0300

    Added SpriteBatch :D

--- core/src/com/pocketbattles/game/Utilities/Utilities.java
+++ core/src/com/pocketbattles/game/Utilities/Utilities.java
@@ -1,21 +1,24 @@
 package com.pocketbattles.game.Utilities;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 /**
  * Created by Boris on 16.08.2016.
  * Project: PocketBattlesV4
  */
 public class Utilities {
+    public static SpriteBatch spriteBatch;
 
     /** INITIALISING */
 
     public static void initialise() {
-
+        spriteBatch = new SpriteBatch();
     }
 
     /** CREATING AND SETTING UP */
 
     public static void create() {
-
+        spriteBatch.enableBlending();
     }
 
     /** UPDATING */
@@ -27,6 +30,6 @@ public class Utilities {
     /** DISPOSING */
 
     public static void dispose() {
-
+        spriteBatch.dispose();
     }
 }
