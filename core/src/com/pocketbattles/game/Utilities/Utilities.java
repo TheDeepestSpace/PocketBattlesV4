628599eef9acf8f3e42d11e976cd5c33571af5c2
Boris <borismilkman@gmail.com>
2016-08-20 02:17:34 +0300

    Added Random :D

--- core/src/com/pocketbattles/game/Utilities/Utilities.java
+++ core/src/com/pocketbattles/game/Utilities/Utilities.java
@@ -2,17 +2,21 @@ package com.pocketbattles.game.Utilities;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.Random;
+
 /**
  * Created by Boris on 16.08.2016.
  * Project: PocketBattlesV4
  */
 public class Utilities {
     public static SpriteBatch spriteBatch;
+    public static Random random;
 
     /** INITIALISING */
 
     public static void initialise() {
         spriteBatch = new SpriteBatch();
+        random = new Random(System.nanoTime());
     }
 
     /** CREATING AND SETTING UP */
