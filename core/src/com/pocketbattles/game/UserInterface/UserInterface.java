c75d7d14bd525f098a04b9efcce422ee3028b4a4
Boris <borismilkman@gmail.com>
2016-08-21 05:23:11 +0300

    Added texture atlas and skin :D

--- core/src/com/pocketbattles/game/UserInterface/UserInterface.java
+++ core/src/com/pocketbattles/game/UserInterface/UserInterface.java
@@ -2,6 +2,8 @@ package com.pocketbattles.game.UserInterface;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 /**
  * Created by Boris on 16.08.2016.
@@ -9,12 +11,18 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
  */
 public class UserInterface {
     public static BitmapFont defaultFont;
+    public static TextureAtlas atlas;
+    public static Skin skin;
 
     /** INITIALISING */
 
     public static void initialise() {
         String defaultFontPath = "UserInterface/Fonts/defaultFont/font16.fnt";
         defaultFont = new BitmapFont(Gdx.files.internal(defaultFontPath));
+        String atlasPath = "UserInterface/Atlas/atlas.pack";
+        atlas = new TextureAtlas(Gdx.files.internal(atlasPath));
+        String skinPath = "UserInterface/Skin/skin.json";
+        skin = new Skin(Gdx.files.internal(skinPath), atlas);
     }
 
     /** CREATING AND SETTING UP */
@@ -39,5 +47,7 @@ public class UserInterface {
 
     public static void dispose() {
         defaultFont.dispose();
+        atlas.dispose();
+        skin.dispose();
     }
 }
