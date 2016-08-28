557cdec501de9fb4dcd82a21a9109c973310fe22
Boris <borismilkman@gmail.com>
2016-08-28 05:22:08 +0300

    Tested TextButton :D

--- core/src/com/pocketbattles/game/UserInterface/Screens/MainScreen/UI.java
+++ core/src/com/pocketbattles/game/UserInterface/Screens/MainScreen/UI.java
@@ -1,7 +1,9 @@
 package com.pocketbattles.game.UserInterface.Screens.MainScreen;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.pocketbattles.game.UserInterface.Actors.Label;
+import com.pocketbattles.game.UserInterface.Actors.TextButton;
 import com.pocketbattles.game.UserInterface.UserInterface;
 
 /**
@@ -14,6 +16,7 @@ public class UI {
 
     public static void setUp() {
         UserInterface.screenTable.reset();
-        UserInterface.screenTable.add(Label.addInstance("MAIN_SCREEN_TITLE", "POCKET BATTLES V4", "fancyFont64", Color.GREEN));
+        UserInterface.screenTable.add(Label.addInstance("MAIN_SCREEN_TITLE", "POCKET BATTLES V4", "fancyFont64", Color.GREEN)).row();
+        UserInterface.screenTable.add(TextButton.addInstance("MAIN_SCREEN_TEST_BUTTON", "TEST", 40, new ClickListener()));
     }
 }
