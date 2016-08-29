19e3489277ebb3666e243ef820bcb675efa8f7e3
Boris <borismilkman@gmail.com>
2016-08-29 16:51:53 +0300

    Improved Upgrade Screen UI :D

--- core/src/com/pocketbattles/game/UserInterface/Screens/UpgradeScreen/UI.java
+++ core/src/com/pocketbattles/game/UserInterface/Screens/UpgradeScreen/UI.java
@@ -2,6 +2,8 @@ package com.pocketbattles.game.UserInterface.Screens.UpgradeScreen;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.pocketbattles.game.Game;
@@ -26,12 +28,45 @@ public class UI {
     /** CREATING AND SETTING */
 
     public static void setUp() {
-        localScreenTable.add().width(100);
-        localScreenTable.add(Label.addInstance("UPGRADE_SCREEN_TITLE", "UPGRADE", "fancyFont32", Color.GREEN));
-        localScreenTable.add().width(100).row();
-        localScreenTable.add().height(300);
         localScreenTable.add();
+        localScreenTable.add(Label.addInstance("UPGRADE_SCREEN_TITLE", "UPGRADE", "fancyFont32", Color.GREEN));
+        localScreenTable.add().row();
+        localScreenTable.add().height(50).row();
+        localScreenTable.add().height(300);
+
+        Table entityListTable = new Table(UserInterface.skin);
+        entityListTable.add(TextButton.addInstance("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY1", "ENTITY1", "toggle", 40, new ClickListener())).fill().row();
+        entityListTable.add(TextButton.addInstance("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY2", "ENTITY2", "toggle", 40, new ClickListener())).fill().row();
+        entityListTable.add(TextButton.addInstance("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY3", "ENTITY3", "toggle", 40, new ClickListener())).fill().row();
+        entityListTable.add(TextButton.addInstance("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY4", "ENTITY4", "toggle", 40, new ClickListener())).fill().row();
+        entityListTable.add(TextButton.addInstance("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY5", "ENTITY5", "toggle", 40, new ClickListener())).fill().row();
+        entityListTable.add(TextButton.addInstance("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY6", "ENTITY6", "toggle", 40, new ClickListener())).fill().row();
+        entityListTable.add(TextButton.addInstance("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY7", "ENTITY7", "toggle", 40, new ClickListener())).fill().row();
+        entityListTable.add(TextButton.addInstance("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY8", "ENTITY8", "toggle", 40, new ClickListener())).fill().row();
+
+        ButtonGroup entityListButtonGroup = new ButtonGroup(
+                TextButton.getButton("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY1"),
+                TextButton.getButton("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY2"),
+                TextButton.getButton("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY3"),
+                TextButton.getButton("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY4"),
+                TextButton.getButton("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY5"),
+                TextButton.getButton("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY6"),
+                TextButton.getButton("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY7"),
+                TextButton.getButton("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY8")
+        );
+        entityListButtonGroup.setMaxCheckCount(1);
+        entityListButtonGroup.setMinCheckCount(0);
+        entityListButtonGroup.setUncheckLast(true);
+
+        ScrollPane entityListScrollPane = new ScrollPane(entityListTable, UserInterface.skin);
+
+        Table upgradesTable = new Table(UserInterface.skin);
+        upgradesTable.add(entityListScrollPane);
+        upgradesTable.add().width(350);
+
+        localScreenTable.add(upgradesTable).height(300);
         localScreenTable.add(Label.addInstance("UPGRADE_SCREEN_GOLD_AMOUNT", "fancyFont16", Color.GOLD)).left().row();
+        localScreenTable.add().height(50).row();
         localScreenTable.add();
         localScreenTable.add(TextButton.addInstance("UPGRADE_SCREEN_BACK_TO_MAIN_SCREEN_BUTTON", "BACK", 40, new ClickListener() {
             @Override
