9c63b82cee43fbe8f2d7292db200220617b9d84b
Boris <borismilkman@gmail.com>
2016-10-19 21:13:54 +0300

    Fit in line :D

--- core/src/com/pocketbattles/game/UserInterface/Screens/UpgradeScreen/UI.java
+++ core/src/com/pocketbattles/game/UserInterface/Screens/UpgradeScreen/UI.java
@@ -20,63 +20,59 @@ import com.pocketbattles.game.UserInterface.UserInterface;
 public class UI {
     private static Table localScreenTable;
 
-    private static float upgradeEntityImageMultiplier;
-
     /** INITIALISING */
 
     public static void initialise() {
         localScreenTable = new Table(UserInterface.skin);
-
-        upgradeEntityImageMultiplier = 1f;
     }
 
     /** CREATING AND SETTING */
 
     public static void setUp() {
         localScreenTable.add();
-        localScreenTable.add(Label.addInstance("UPGRADE_SCREEN_TITLE", "UPGRADE", "fancyFont32", Color.GREEN));
+        localScreenTable.add(
+                Label.addInstance("UPGRADE_SCREEN_TITLE", "UPGRADE", "fancyFont32", Color.GREEN)
+        );
         localScreenTable.add().row();
         localScreenTable.add().height(50).row();
         localScreenTable.add().height(300);
 
         final Table upgradesTableImageTable = new Table(UserInterface.skin);
-        upgradesTableImageTable.add(Image.addInstance("UPGRADE_SCREEN_UPGRADABLE_ENTITY_IMAGE", "image.png"));
+        upgradesTableImageTable.add(
+                Image.addInstance("UPGRADE_SCREEN_UPGRADABLE_ENTITY_IMAGE", "badlogic.jpg")
+        );
 
         Table entityListTable = new Table(UserInterface.skin);
-        entityListTable.add(TextButton.addInstance("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY1", "ENTITY1", "toggle", 40, new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                Image.getImage("UPGRADE_SCREEN_UPGRADABLE_ENTITY_IMAGE").setTexture("image.png");
-            }
-        })).fill().row();
-        entityListTable.add(TextButton.addInstance("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY2", "ENTITY2", "toggle", 40, new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                Image.getImage("UPGRADE_SCREEN_UPGRADABLE_ENTITY_IMAGE").setTexture("image2.png");
-            }
-        })).fill().row();
-        entityListTable.add(TextButton.addInstance("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY3", "ENTITY3", "toggle", 40, new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                Image.getImage("UPGRADE_SCREEN_UPGRADABLE_ENTITY_IMAGE").setTexture("image3.png");
-            }
-        })).fill().row();
-        entityListTable.add(TextButton.addInstance("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY4", "ENTITY4", "toggle", 40, new ClickListener())).fill().row();
-        entityListTable.add(TextButton.addInstance("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY5", "ENTITY5", "toggle", 40, new ClickListener())).fill().row();
-        entityListTable.add(TextButton.addInstance("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY6", "ENTITY6", "toggle", 40, new ClickListener())).fill().row();
-        entityListTable.add(TextButton.addInstance("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY7", "ENTITY7", "toggle", 40, new ClickListener())).fill().row();
-        entityListTable.add(TextButton.addInstance("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY8", "ENTITY8", "toggle", 40, new ClickListener())).fill().row();
+        for (int i = 0; i < Game.entityClassesNames.size(); i++) {
+            final int finalI = i;
+            entityListTable.add(
+                    TextButton.addInstance(
+                            "UPGRADE_SCREEN_ENTITY_LIST_TABLE_" + Game.entityClassesNames.get(i),
+                            Game.entityClassesNames.get(i),
+                            "toggle",
+                            40,
+                            new ClickListener() {
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    Image.getImage(
+                            "UPGRADE_SCREEN_UPGRADABLE_ENTITY_IMAGE")
+                            .setTexture(
+                                    "Entities/"
+                                            + Game.entityClassesNames.get(finalI)
+                                            + "/initial.png"
+                            );
+                }
+            })).fill().row();
+        }
 
-        ButtonGroup entityListButtonGroup = new ButtonGroup(
-                TextButton.getButton("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY1"),
-                TextButton.getButton("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY2"),
-                TextButton.getButton("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY3"),
-                TextButton.getButton("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY4"),
-                TextButton.getButton("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY5"),
-                TextButton.getButton("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY6"),
-                TextButton.getButton("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY7"),
-                TextButton.getButton("UPGRADE_SCREEN_ENTITY_LIST_TABLE_ENTITY8")
-        );
+        ButtonGroup entityListButtonGroup = new ButtonGroup();
+        for (int i = 0; i < Game.entityClassesNames.size(); i++) {
+            entityListButtonGroup.add(
+                    TextButton.getButton(
+                            "UPGRADE_SCREEN_ENTITY_LIST_TABLE_" + Game.entityClassesNames.get(i)
+                    )
+            );
+        }
         entityListButtonGroup.setMaxCheckCount(1);
         entityListButtonGroup.setMinCheckCount(0);
         entityListButtonGroup.setUncheckLast(true);
@@ -88,10 +84,19 @@ public class UI {
         upgradesTable.add(upgradesTableImageTable).width(200).fill();
 
         localScreenTable.add(upgradesTable).height(300);
-        localScreenTable.add(Label.addInstance("UPGRADE_SCREEN_GOLD_AMOUNT", "fancyFont16", Color.GOLD)).left().row();
+        localScreenTable.add(
+                Label.addInstance(
+                        "UPGRADE_SCREEN_GOLD_AMOUNT", "fancyFont16", Color.GOLD
+                )
+        ).left().row();
         localScreenTable.add().height(50).row();
         localScreenTable.add();
-        localScreenTable.add(TextButton.addInstance("UPGRADE_SCREEN_BACK_TO_MAIN_SCREEN_BUTTON", "BACK", 40, new ClickListener() {
+        localScreenTable.add(
+                TextButton.addInstance(
+                        "UPGRADE_SCREEN_BACK_TO_MAIN_SCREEN_BUTTON",
+                        "BACK",
+                        40,
+                        new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 PocketBattles.upgradeScreen.game.setMainScreen();
