4779f18225a254d6add50da5fc2a82561bc0cbff
Boris <borismilkman@gmail.com>
2016-12-12 20:29:28 +0300

    Added SWORDSMAN class  :D

---
+++ core/src/com/pocketbattles/game/Actors/Entity/Leaf/SWORDSMAN.java
@@ -0,0 +1,39 @@
+package com.pocketbattles.game.Actors.Entity.Leaf;
+
+import com.pocketbattles.game.Actors.Entity.Base.Entity;
+
+/**
+ * Created by Boris on 23.10.2016.
+ * Project: PocketBattlesV4
+ */
+public class SWORDSMAN extends Entity {
+
+    /** INITIALISING */
+
+    public static void initialiseClass() {
+        levels = 5;
+        levelsAvailable = 1;
+    }
+
+    SWORDSMAN() {
+        super();
+    }
+
+    /** CREATING AND SETTING UP */
+    @Override
+    protected void setUp(int line, int row, Entity.Team team) {
+        super.setUp(line, row, team);
+
+        this.initHP = this.HP = 100;
+    }
+
+    /** UPDATING */
+
+    /** GETTERS / SETTERS */
+
+    /** INTERACTING */
+
+    /** RENDERING */
+
+    /** DISPOSING */
+}
