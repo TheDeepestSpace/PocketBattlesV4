b3a7976e94e591dafce7c903eebe642b08541a24
Boris <borismilkman@gmail.com>
2016-12-15 21:36:49 +0300

    Added User class for purchases :D

---
+++ core/src/com/pocketbattles/game/Actors/User.java
@@ -0,0 +1,62 @@
+package com.pocketbattles.game.Actors;
+
+import com.pocketbattles.game.Actors.Entity.Base.ReflectionUtility;
+import com.pocketbattles.game.Game;
+
+/**
+ * Created by Boris on 12.12.2016.
+ * Project: PocketBattlesV4
+ */
+public class User {
+
+    /** INITIALISING */
+    public static void initialise() {
+
+    }
+
+    /** CREATING AND SETTING UP */
+
+    public static void setUp() {
+
+    }
+
+    /** GETTERS / SETTERS */
+
+    /** UPDATING */
+
+    /** INTERACTING */
+
+    public static boolean enoughFor(int cost) {
+        return cost <= Game.GOLD_AMOUNT;
+    }
+
+    public static void pay(int cost) {
+        Game.GOLD_AMOUNT -= cost;
+    }
+
+    public static boolean buyUpgrade(String entityClass) {
+        int upgradeCost = ReflectionUtility.invoke(entityClass, "getUpgradeCost");
+        if (enoughFor(upgradeCost)) {
+            pay(upgradeCost);
+            ReflectionUtility.invoke(entityClass, "addLevelsAvailable");
+            return true;
+        }else return false;
+
+    }
+
+    public static boolean buyEntity(String entityClass, int level) {
+        int buyCost = ReflectionUtility.invoke(entityClass, "getCost", level);
+
+        if (enoughFor(buyCost)) {
+            pay(buyCost);
+            ReflectionUtility.invoke(entityClass, "addEntity", level);
+            return true;
+        }else return false;
+    }
+
+    /** DISPOSING / RESETTING */
+
+    public static void dispose() {
+
+    }
+}
